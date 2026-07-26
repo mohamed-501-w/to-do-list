c786a516467bf41dd6908678425541d769d0fe0f
mohamed-501-w <mohamedwaled501@gmail.com>
2026-07-26 21:39:27 +0300

    ....

--- BackEnd/src/controllers/userControllers.js
+++ BackEnd/src/controllers/userControllers.js
@@ -57,7 +57,7 @@ export const login = async (req, res) => {
         res.cookie("refreshToken", refreshToken, {
             httpOnly: true,
             secure: true,
-            sameSite: "none",
+            sameSite: "strict",
             maxAge: SEVEN_DAYS_MS,
         })
         res.json({accessToken})
